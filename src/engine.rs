c09f26695e30e38c7de7d918c69f342d37b963da
CadenMG <cadengarrett@gmail.com>
2021-04-05 20:54:50 -0400

    Add minimax for move evaluation

--- src/engine.rs
+++ src/engine.rs
@@ -1,4 +1,4 @@
-use chess::{Game, ChessMove, MoveGen};
+use chess::{Game, ChessMove, MoveGen, Board, BoardStatus};
 
 pub struct Engine {
     game_state: Game,
@@ -16,7 +16,41 @@ impl Engine {
     }
 
     pub fn get_best_move(&self) -> Option<ChessMove> {
-        let mut move_gen = MoveGen::new_legal(&self.game_state.current_position());
-        return move_gen.next();
+        let move_and_value = MoveGen::new_legal(&self.game_state.current_position())
+            .map(|a_move| (a_move, minimax(self.game_state.current_position().make_move_new(a_move), 2, true)));
+        let mut best_move = (Option::None, -1);
+        for (chess_move, value) in move_and_value {
+            if value > best_move.1 { best_move = (Option::Some(chess_move), value) }
+        }
+        return best_move.0;
     }
+}
+
+fn minimax(position: Board, depth: i32, maximizing_player: bool) -> i32 {
+    if depth == 0 || position.status() != BoardStatus::Ongoing {
+        return heuristic(position);
+    }
+    if maximizing_player {
+        let mut value = -1;
+        let children = MoveGen::new_legal(&position);
+        for child in children {
+            let new_position = position.make_move_new(child);
+            value = std::cmp::max(value, minimax(new_position, depth - 1, false));
+        }
+        return value;
+    }
+    else {
+        let mut value = 101;
+        let children = MoveGen::new_legal(&position);
+        for child in children {
+            let new_position = position.make_move_new(child);
+            value = std::cmp::min(value, minimax(new_position, depth - 1, true));
+        }
+        return value;
+    }
+}
+
+fn heuristic(position: Board) -> i32 {
+    //return rand::thread_rng().gen_range(0..100);
+    return 10;
 }
