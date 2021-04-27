a7d4d8262ee59198b3e66ef4619c028269f2ed52
CadenMG <cadengarrett@gmail.com>
2021-04-26 21:46:40 -0400

    Impl bstar

--- src/engine.rs
+++ src/engine.rs
@@ -34,7 +34,7 @@ impl Engine {
 
     pub fn get_best_move(&self) -> Option<ChessMove> {
         let move_and_value = MoveGen::new_legal(&self.game_state.current_position())
-            .map(|a_move| (a_move, alphabeta(self.game_state.current_position().make_move_new(a_move), 5, MIN_VAL, MAX_VAL, true)));
+            .map(|a_move| (a_move, bstar(self.game_state.current_position().make_move_new(a_move), 3, true)));
         let mut best_move = (Option::None, MIN_VAL);
         for (chess_move, value) in move_and_value {
             if value > best_move.1 { best_move = (Option::Some(chess_move), value) }
@@ -107,10 +107,34 @@ fn bestfirst(position: Board, depth: i32, maximizing_player: bool) -> i32 {
 
     let mut best_move = (Option::None, MIN_VAL);
     for (chess_move, value) in moves_and_values {
-        if value > best_move.1 { best_move = (Option::Some(chess_move), alphabeta(position.make_move_new(chess_move), 2, MIN_VAL,MAX_VAL, true)) }
+        if value > best_move.1 { best_move = (Option::Some(chess_move), alphabeta(position.make_move_new(chess_move), depth, MIN_VAL,MAX_VAL, maximizing_player)) }
     }
     return best_move.1;
 }
+
+fn bstar(position: Board, depth: i32, maximizing_player: bool) -> i32 {
+    let moves: Vec<ChessMove> = MoveGen::new_legal(&position).collect();
+    let mut moves_and_values: Vec<(ChessMove, i32)> = Vec::new();
+    for mv in moves {
+        let pos_after_first = position.make_move_new(mv);
+        let pos_after_null = pos_after_first.null_move().unwrap_or(pos_after_first);
+        let next_moves: Vec<ChessMove> = MoveGen::new_legal(&pos_after_null).collect();
+        let mut values: Vec<i32> = Vec::new();
+        for next_mv in next_moves {
+            values.push(heuristic(pos_after_null.make_move_new(next_mv)));
+        }
+        moves_and_values.push((mv, *values.iter().max().unwrap_or(&0)));
+    }
+
+    moves_and_values.sort_by(|a, b| a.1.cmp(&b.1));
+
+    let mut best_move = (Option::None, MIN_VAL);
+    for (chess_move, value) in moves_and_values {
+        if value > best_move.1 { best_move = (Option::Some(chess_move), alphabeta(position.make_move_new(chess_move), depth, MIN_VAL,MAX_VAL, maximizing_player)) }
+    }
+    return best_move.1;
+}
+
 
 fn randomheuristic(position: Board) -> i32 {
     let mut rng = rand::thread_rng();
